b8d79ba6c05742b5e62d80e05935513c59565be6
taras_2409 <kolesnichenko_7@mail.ru>
2012-01-18 16:50:29 +0200

    111

---
+++ src/app/pack/helloworld4/HelloWorld_4Activity.java
@@ -0,0 +1,13 @@
+package app.pack.helloworld4;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelloWorld_4Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
